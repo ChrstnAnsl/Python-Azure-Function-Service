b1c3ce86adef86041b1d5c1c255a07cf1daf7b59
Christian <36796016+ChrstnAnsl@users.noreply.github.com>
2023-08-14 13:03:51 +0800

    fix script

--- CronJobAlertNotification/__init__.py
+++ CronJobAlertNotification/__init__.py
@@ -1,7 +1,11 @@
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 import azure.functions as func
 import logging
 import requests
+import pytz
+
+log_file = "logfile.log"
+est_timezone = pytz.timezone('America/New_York')
 
 def send_notification_to_teams() -> None:
     try:
@@ -21,6 +25,19 @@ def main(mytimer: func.TimerRequest) -> None:
     if mytimer.past_due:
         logging.warning('The timer is past due!')
 
-    send_notification_to_teams()
+    # Get the current day of the week (0 = Monday, 1 = Tuesday, ..., 4 = Friday)
+    current_day = datetime.now(est_timezone).weekday()
+
+    # Check if it's the last weekday of the month
+    if (datetime.now(est_timezone) + timedelta(days=1)).month != datetime.now(est_timezone).month and current_day < 5:
+        with open(log_file, "a") as file:
+            file.write(f"{datetime.now(est_timezone)}: log for this timesheet as End of Month\n")
+        send_notification_to_teams()
+
+    # Check if it's Friday (0 = Monday, ..., 4 = Friday)
+    if current_day == 4:
+        with open(log_file, "a") as file:
+            file.write(f"{datetime.now(est_timezone)}: log for this timesheet as end of Week\n")
+        send_notification_to_teams()
 
     logging.info('Python timer trigger function ran at %s', utc_timestamp)
